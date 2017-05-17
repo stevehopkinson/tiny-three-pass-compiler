87f61ce9b672f7713d82c37029a2cc1143aa8478
Steve Hopkinson <steve@stevehopkinson.co.uk>
2017-05-17 12:52:22 +0100

    Build first pass

--- compiler.js
+++ compiler.js
@@ -16,6 +16,112 @@ Compiler.prototype.tokenize = function (program) {
 
 Compiler.prototype.pass1 = function (program) {
   var tokens = this.tokenize(program);
+
+  function getNextToken ()
+  {
+    token = tokens.shift();
+  }
+
+  function precedenceIsNotGreater (o1, o2)
+  {
+    var precedences = {
+      '/' : 4,
+      '*' : 3,
+      '+' : 2,
+      '-' : 1,
+    }
+    return precedences[o1] <= precedences[o2];
+  }
+
+  function isNumber (token) {
+    return !isNaN(token);
+  }
+
+  function isOperator (token) {
+    return "*/+-".indexOf(token) !== -1;
+  }
+
+  var token;
+  var outputQueue = [];
+  var operatorStack = [];
+  var args = [];
+
+  do {
+    getNextToken();
+    if (token === '[') {
+      for (
+        getNextToken();
+        token !== ']';
+        getNextToken()
+      ) {
+        args.push(token);
+      }
+    }
+    else if (isNumber(token) || args.includes(token)) {
+      outputQueue.push(token);
+    }
+    else if (isOperator(token)) {
+      var o1 = token;
+      for (
+        var o2 = operatorStack[operatorStack.length - 1]; 
+        operatorStack.length && isOperator(o2) && precedenceIsNotGreater(o1, o2); 
+        o2 = operatorStack[operatorStack.length - 1]
+      ) {
+        outputQueue.push(operatorStack.pop());
+      }
+      operatorStack.push(o1);
+    }
+    else if (token === '(') {
+      operatorStack.push(token);
+    }
+    else if (token === ')') {
+      for (
+        var nextOperator = operatorStack[operatorStack.length - 1]; 
+        operatorStack.length && nextOperator !== '('; 
+        nextOperator = operatorStack[operatorStack.length - 1]
+      ) {
+        outputQueue.push(operatorStack.pop())
+      }
+      operatorStack.pop();
+    }
+  } while (tokens.length);
+
+  while (operatorStack.length) {
+    outputQueue.push(operatorStack.pop())
+  }
+
+  var output;
+
+  function getNextOutput () {
+    output = outputQueue.pop();
+  }
+
+  function buildAbstractSyntaxTree (outputQueue) {
+    getNextOutput();
+    var node = {};
+
+    if (isNumber(output)) {
+      node.op = 'imm';
+      node.n = output;
+    } 
+    else if (args.includes(output)) {
+      node.op = 'arg';
+      node.n = args.indexOf(output);
+    }
+    else if (isOperator(output)) {
+      node.op = output;
+      var b = buildAbstractSyntaxTree(outputQueue);
+      var a = buildAbstractSyntaxTree(outputQueue);
+      node.a = a;
+      node.b = b;
+    }
+
+    return node;
+  }
+
+  var abstractSyntaxTree = buildAbstractSyntaxTree(outputQueue);
+  console.log(JSON.stringify(abstractSyntaxTree));
+  return buildAbstractSyntaxTree(outputQueue);
 };
 
 Compiler.prototype.pass2 = function (ast) {
@@ -25,3 +131,5 @@ Compiler.prototype.pass2 = function (ast) {
 Compiler.prototype.pass3 = function (ast) {
   // return assembly instructions
 };
+
+module.exports = Compiler;
