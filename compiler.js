dbb8d0069dd7af7e2950caebb615664c9ad6f8ac
Steve Hopkinson <steve@stevehopkinson.co.uk>
2017-05-17 09:57:00 +0100

    Add starting code

---
+++ compiler.js
@@ -0,0 +1,27 @@
+function Compiler () {};
+
+Compiler.prototype.compile = function (program) {
+  return this.pass3(this.pass2(this.pass1(program)));
+};
+
+Compiler.prototype.tokenize = function (program) {
+  // Turn a program string into an array of tokens.  Each token
+  // is either '[', ']', '(', ')', '+', '-', '*', '/', a variable
+  // name or a number (as a string)
+  var regex = /\s*([-+*/\(\)\[\]]|[A-Za-z]+|[0-9]+)\s*/g;
+  return program.replace(regex, ":$1").substring(1).split(':').map( function (tok) {
+    return isNaN(tok) ? tok : tok|0;
+  });
+};
+
+Compiler.prototype.pass1 = function (program) {
+  var tokens = this.tokenize(program);
+};
+
+Compiler.prototype.pass2 = function (ast) {
+  // return AST with constant expressions reduced
+};
+
+Compiler.prototype.pass3 = function (ast) {
+  // return assembly instructions
+};
