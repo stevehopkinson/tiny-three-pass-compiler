d6acb47337291fea608a37f334fa357acc23156b
Steve Hopkinson <steve@stevehopkinson.co.uk>
2017-05-17 16:40:39 +0100

    Update tests for third pass

--- compiler-test.js
+++ compiler-test.js
@@ -1,14 +1,34 @@
 var Compiler = require('./compiler.js');
 
-// Note: the simulate(asm,args) function is pre-loaded.
 
-var prog = '[ x y z ] ( 2*3*x + 5*y - 3*z ) / (1 + 3 + 2*2)';
-var t1 = JSON.stringify({"op":"/","a":{"op":"-","a":{"op":"+","a":{"op":"*","a":{"op":"*","a":{"op":"imm","n":2},"b":{"op":"imm","n":3}},"b":{"op":"arg","n":0}},"b":{"op":"*","a":{"op":"imm","n":5},"b":{"op":"arg","n":1}}},"b":{"op":"*","a":{"op":"imm","n":3},"b":{"op":"arg","n":2}}},"b":{"op":"+","a":{"op":"+","a":{"op":"imm","n":1},"b":{"op":"imm","n":3}},"b":{"op":"*","a":{"op":"imm","n":2},"b":{"op":"imm","n":2}}}});
-var t2 = JSON.stringify({"op":"/","a":{"op":"-","a":{"op":"+","a":{"op":"*","a":{"op":"imm","n":6},"b":{"op":"arg","n":0}},"b":{"op":"*","a":{"op":"imm","n":5},"b":{"op":"arg","n":1}}},"b":{"op":"*","a":{"op":"imm","n":3},"b":{"op":"arg","n":2}}},"b":{"op":"imm","n":8}});
+function simulate(asm, args) {
+  var r0 = undefined;
+  var r1 = undefined;
+  var stack = [];
+  asm.forEach(function (instruct) {
+    var match = instruct.match(/(IM|AR)\s+(\d+)/) || [ 0, instruct, 0 ];
+    var ins = match[1];
+    var n = match[2] | 0;
+
+    if (ins == 'IM')   { r0 = n; }
+    else if (ins == 'AR') { r0 = args[n]; }
+    else if (ins == 'SW') { var tmp = r0; r0 = r1; r1 = tmp; }
+    else if (ins == 'PU') { stack.push(r0); }
+    else if (ins == 'PO') { r0 = stack.pop(); }
+    else if (ins == 'AD') { r0 += r1; }
+    else if (ins == 'SU') { r0 -= r1; }
+    else if (ins == 'MU') { r0 *= r1; }
+    else if (ins == 'DI') { r0 /= r1; }
+  });
+  return r0;
+}
 
 var c = new Compiler();
 
-var p1 = c.pass1(prog);
+var prog = '[ x y z ] x - y - z + 10 / 5 / 2 - 7 / 1 / 7';
+p1 = c.pass1(prog);
+p2 = c.pass2(p1);
+p3 = c.pass3(p2);
 
-var p2 = c.pass2(p1);
-console.log(p2);
+console.log('Pass 3: ' + JSON.stringify(p3));
+console.log('Result: ' + simulate(p3, [5, 4, 1]));
